f11fc3c1a6f07ccf1d8240a48146396db63ef7b3
Ian Hayward <ian.hayward@havasww.com>
2019-11-18 20:39:58 +0000

    Small updates

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ module.exports = {
   author: 'Ian Hayward', // Required
   description: 'The blog of a tech lead, equal parts tutorial and archive for things I have worked on.',
   additionalDescription: 'I don\'t do adverts and would rather a free crypto viewing model. If you are interested in either how that works or helping me out, click on the Brave or BitTube links below',
-  primaryColor: '#1d2456', // Required
+  primaryColor: '#00645C', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: '../index.html',
+    website: 'mailto:me@ijh.dev',
     github: 'https://github.com/seperot',
-    twitter: 'https://twitter.com/Seperot',
+    twitter: 'https://twitter.com/ijhdev',
     linkedin: 'https://www.linkedin.com/in/ianjhayward',
-    brave: 'https://brave.com/ijh083',
+    brave: 'https://brave.com/ijh738',
     bittube: 'https://bittubeapp.com/?ref?2JQDVSX8J'
   },
   pathPrefix: '/devblog',
